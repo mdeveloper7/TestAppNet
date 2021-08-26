1fe95e51240b09742877f51907334fee49b1cec7
Miguel Rodriguez <rdz.miguel@live.com>
2021-08-25 20:56:51 -0500

    Add project files.

---
+++ TestApp/Controllers/MoviesController.cs
@@ -0,0 +1,117 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+using TestApp.Models;
+
+namespace TestApp.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class MoviesController : ApiController
+    {
+        private MoviesEntities db = new MoviesEntities();
+
+        // GET: api/Movies
+        public IQueryable<Movie> GetMovies()
+        {
+            return db.Movies;
+        }
+
+        // GET: api/Movies/5
+        [ResponseType(typeof(Movie))]
+        public async Task<IHttpActionResult> GetMovie(int id)
+        {
+            Movie movie = await db.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(movie);
+        }
+
+        // PUT: api/Movies/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutMovie(int id, Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != movie.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(movie).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Movies
+        [ResponseType(typeof(Movie))]
+        public async Task<IHttpActionResult> PostMovie(Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Movies.Add(movie);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = movie.Id }, movie);
+        }
+
+        // DELETE: api/Movies/5
+        [ResponseType(typeof(Movie))]
+        public async Task<IHttpActionResult> DeleteMovie(int id)
+        {
+            Movie movie = await db.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            db.Movies.Remove(movie);
+            await db.SaveChangesAsync();
+
+            return Ok(movie);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool MovieExists(int id)
+        {
+            return db.Movies.Count(e => e.Id == id) > 0;
+        }
+    }
+}
